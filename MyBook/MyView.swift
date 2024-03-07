3ace1995c25d5a3c7496ee44e58fee6c3688bd9d
Artur Knyazev <grazhdaninnevalashka@gmail.com>
2024-03-07 20:11:00 +0300

    Add - project start files

---
+++ MyBook/MyView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MyView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- MyBook/ViewController.swift
+++ MyBook/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  MyBook
-//
-//  Created by Artur on 07.03.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let myView = MyView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = myView
     }
-
 
 }
 
