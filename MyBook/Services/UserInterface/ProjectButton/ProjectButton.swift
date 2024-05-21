ae77db2041854c3b88ba0291ace7ffe9fc53d8fe
Artur Knyazev <grazhdaninnevalashka@gmail.com>
2024-05-21 14:39:21 +0300

    Add: public method for adding an action to a ProjectButton

--- MyBook/Services/UserInterface/ProjectButton/ProjectButton.swift
+++ MyBook/Services/UserInterface/ProjectButton/ProjectButton.swift
@@ -5,25 +5,29 @@ class ProjectButton: UIButton, ProjectButtonProtocol {
     
     let publisher: AnyPublisher<Void, Never>
     
-    private let externalDataPublisher: PassthroughSubject<Void, Never>
+    private let internalPublisher: PassthroughSubject<Void, Never>
     
     init(_ touchEvent: UIControl.Event) {
-        self.externalDataPublisher = PassthroughSubject<Void, Never>()
-        self.publisher = AnyPublisher(externalDataPublisher)
+        self.internalPublisher = PassthroughSubject<Void, Never>()
+        self.publisher = AnyPublisher(internalPublisher)
         super.init(frame: .zero)
-        addTarget(self, action: #selector(tapButton), for: touchEvent)
+        addTarget(touchEvent)
+    }
+    
+    @objc private func tapButton() {
+        internalPublisher.send()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func removeTarget(_ touchEvent: UIControl.Event) {
-        removeTarget(self, action: #selector(tapButton), for: touchEvent)
+    func addTarget(_ touchEvent: UIControl.Event) {
+        addTarget(self, action: #selector(tapButton), for: touchEvent)
     }
     
-    @objc private func tapButton() {
-        externalDataPublisher.send()
+    func removeTarget(_ touchEvent: UIControl.Event) {
+        removeTarget(self, action: #selector(tapButton), for: touchEvent)
     }
     
 }
--- MyBook/Services/UserInterface/ProjectButton/ProjectButtonProtocol.swift
+++ MyBook/Services/UserInterface/ProjectButton/ProjectButtonProtocol.swift
@@ -3,5 +3,6 @@ import Combine
 
 protocol ProjectButtonProtocol: UIButton {
     var publisher: AnyPublisher<Void, Never> { get }
+    func addTarget(_ touchEvent: UIControl.Event)
     func removeTarget(_ touchEvent: UIControl.Event)
 }
